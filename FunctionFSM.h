d9458cf36b8770efc311c20c477ae44b9f06a635
James Veale <39330986+JRVeale@users.noreply.github.com>
2019-05-08 20:43:51 +0100

    Initial upload

---
+++ FunctionFSM.h
@@ -0,0 +1,95 @@
+/*------------------------------------------------*\
+ * FunctionFSM                                    *
+ * By JRVeale                                     *
+ * 7th May 2019                                   *
+ *                                                *
+ * An FSM libary based upon arduino-fsm that uses *
+ * std::function<void()> instead of raw function  *
+ * pointers. This will allow FSMs to be used      *
+ * parent classes.                                *
+ *                                                *
+ * (c) James Veale 2019                           *
+\*================================================*/
+
+#pragma once
+
+#include <functional>
+#include <vector>
+
+//include Arduino for access to millis() (a millisecond tick count)
+//(this is currently for use on an ESP32, which supports Arduino's libraries)
+//could easily remove if using stdlib's timers instead
+#if defined(ARDUINO) && ARDUINO >= 100
+  #include <Arduino.h>
+#else
+  #include <WProgram.h>
+#endif
+
+
+struct FunctionState {
+  FunctionState(std::function<void()> on_enter,
+                std::function<void()> on_state,
+                std::function<void()> on_exit);
+  std::function<void()> on_enter;
+  std::function<void()> on_state;
+  std::function<void()> on_exit;
+};
+
+class FunctionFsm {
+public:
+  FunctionFsm(FunctionState* initial_state);
+  ~FunctionFsm();
+  
+  void add_transition(FunctionState* state_from,
+                      FunctionState* state_to,
+                      int event,
+                      std::function<void()> on_transition);
+  
+  void add_timed_transition(FunctionState* state_from,
+                            FunctionState* state_to,
+                            unsigned long interval,
+                            std::function<void()> on_transition);
+  
+  void check_timed_transitions();
+  
+  void trigger(int event);
+  void run_machine();
+  
+  FunctionState& get_current_state(){
+    return *m_current_state;
+  }
+  
+  bool is_in_state(FunctionState &s){
+    FunctionState current = get_current_state();
+    return &s == m_current_state;
+  }
+
+private:
+  struct Transition {
+    FunctionState* state_from;
+    FunctionState* state_to;
+    int event;
+    std::function<void()> on_transition;
+  };
+  
+  struct TimedTransition {
+    Transition transition;
+    unsigned long start;
+    unsigned long interval;
+  };
+  
+  static Transition create_transition(FunctionState* state_from,
+                                      FunctionState* state_to,
+                                      int event,
+                                      std::function<void()> on_transition);
+  
+  void make_transition(Transition* transition);
+
+private:
+  FunctionState* m_current_state;
+  
+  std::vector<Transition> m_transitions;
+  std::vector<TimedTransition> m_timed_transitions;
+
+  bool m_initialized;
+};
